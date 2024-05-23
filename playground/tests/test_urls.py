8f13af10c3c6c76781a241bd592f7fbde13b1c19
Nimsha Rajesh <nimsharajesh@Air-von-M-bei-M.fritz.box>
2024-05-23 22:43:58 +0200

    unit testing for urls and views

--- playground/tests/test_urls.py
+++ playground/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from playground.views import home
+from playground.views import home, about, booking, cars, contact, category, help
 
 
 class TestUrls(SimpleTestCase):
@@ -8,4 +8,35 @@ class TestUrls(SimpleTestCase):
     def test_home_joy_url_is_resolved(self):
         url = reverse('home')
         print(resolve(url))
-        self.assertEquals(resolve(url).func, home)
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, home)
+    def test_about_joy_url_is_resolved(self):
+        url = reverse('about')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, about)
+    def test_booking_joy_url_is_resolved(self):
+        url = reverse('booking')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, booking)
+    def test_cars_joy_url_is_resolved(self):
+        url = reverse('cars')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, cars)
+    def test_contact_joy_url_is_resolved(self):
+        url = reverse('contact')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, contact)
+    def test_category_joy_url_is_resolved(self):
+        url = reverse('category')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, category)
+    def test_help_joy_url_is_resolved(self):
+        url = reverse('Help')
+        print(resolve(url))
+        ##self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, help)
--- playground/tests/test_views.py
+++ playground/tests/test_views.py
@@ -7,5 +7,36 @@ class TestViews(TestCase):
   def test_project_home_GET(self):
     client = Client()
     response = client.get(reverse('home'))
-    self.assertEquals(response.status_code, 200)
+    self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'index.html','base.html' )
+  def test_project_about_GET(self):
+    client = Client()
+    response = client.get(reverse('about'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'about.html' )
+  def test_project_booking_GET(self):
+    client = Client()
+    response = client.get(reverse('booking'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'bookings.html' )
+  def test_project_cars_GET(self):
+    client = Client()
+    response = client.get(reverse('cars'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'cars.html' ) 
+  def test_project_contact_GET(self):
+    client = Client()
+    response = client.get(reverse('contact'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'contacts.html' )
+  def test_project_help_GET(self):
+    client = Client()
+    response = client.get(reverse('Help'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'help.html' )
+  def test_project_category_GET(self):
+    client = Client()
+    response = client.get(reverse('category'))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'category.html' )
+  
